3d7671ea6bb469290481b0cd4030a805019e34d1
Slava <slavanew98@mail.ru>
2022-12-13 23:10:21 +0300

    1st exercise is completed

--- exc_1/Program.cs
+++ exc_1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//Тело программы
+
+PrintArray(GenRandomArray(5,5));
+
+
+//методы
+
+double[,] GenRandomArray(int n, int m)
+{
+    double[,] array = new double[n,m];
+    Random rand = new Random();
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            array[i,j] = rand.NextDouble() * 10;
+            array[i,j] = Math.Round(array[i,j], 2);
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
