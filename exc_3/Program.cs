c6fcd9521b93d0a4a6e62b2987e3e57a2a19f307
Slava <slavanew98@mail.ru>
2022-12-15 17:03:49 +0300

    3rd exercise is completed

--- exc_3/Program.cs
+++ exc_3/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+*/
+
+//Тело программы
+
+int[,] local_array = GenRandomArray(5, 5);
+PrintArray(local_array);
+string out_array = string.Empty;
+out_array = string.Join(", ", CalculateArithmeticMean(local_array));
+
+Console.WriteLine($"[{out_array}]");
+
+//Методы
+
+int[,] GenRandomArray(int CountRows, int CountColumns)
+{
+    int[,] array = new int[CountRows, CountColumns];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(0, 10);
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}    ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] CalculateArithmeticMean(int[,] Array)
+{
+    double[] Mean = new double[Array.GetLength(1)];
+    double ColumnsSum = 0.0;
+
+    for (int j = 0; j < Array.GetLength(1); j++)
+    {
+        ColumnsSum = 0.0;
+        for (int i = 0; i < Array.GetLength(0); i++)
+        {
+            ColumnsSum += Array[i, j];
+        }
+        Mean[j] = ColumnsSum / Array.GetLength(0);
+    }
+
+    return Mean;
+}
